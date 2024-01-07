84cee2892dbbb7fa432ded9b709c37c14487f109
Taylan Özdoğan <ozdogantaylan@gmail.com>
2024-01-08 00:33:44 +0300

    indices added.

--- js/script.js
+++ js/script.js
@@ -91,6 +91,16 @@ verticesOfShape = [
     -0.5, -0.5,  0.5,
 ];
 
+const indices = [
+    0, 1, 2,     0, 2, 3,    // Front face
+    4, 5, 6,     4, 6, 7,    // Back face
+    8, 9, 10,    8, 10, 11,  // Top face
+    12, 13, 14,  12, 14, 15, // Bottom face
+    16, 17, 18,  16, 18, 19, // Right face
+    20, 21, 22,  20, 22, 23  // Left face
+];
+
+
 
 const textureCoordinates = [
     // Front
